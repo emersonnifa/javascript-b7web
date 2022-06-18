e030649aab6f15b32a6c9ae66ef776a3254894a4
Emerson Nifa <emersongusta@gmail.com>
2022-06-17 21:14:50 -0300

    js output

---
+++ script1.js
@@ -0,0 +1,10 @@
+// manipulação de elemento
+
+document.getElementById("titulo").innerHTML = "Bora estudar ae doido!"; //através do Id do elemento
+
+document.write("Outro texto na tela"); //criando um elemento na tela
+
+// mostrar elemento através da janela
+
+window.alert("Mensagem de exemplo!"); // ou alert("Mensagem de exemplo!");
+console.log(titulo); // mensagem que aparece na tela inspencionar console
