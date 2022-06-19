b061ce1892b50319242c36f6bb14f2e6f3033e18
Emerson Nifa <emersongusta@gmail.com>
2022-06-19 11:32:19 -0300

    estrutura condicional if and else

---
+++ js/script2.js
@@ -0,0 +1,7 @@
+var nome ="Emerson"; // String
+var sobrenome ="Nifa";
+var idade = 27; // int
+
+var nomecompleto=nome + " " + sobrenome;
+
+alert(nomecompleto + " idade    " + idade);
---
+++ js/script3.js
@@ -0,0 +1,10 @@
+// Condicionais 
+
+var hora = 9;
+
+// start condição 
+if (hora < 12){
+    console.log("Bom dia!");
+}else{
+    console.log("Boa tarde")
+}
