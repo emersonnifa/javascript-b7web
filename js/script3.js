83547617e339605f50144eaa5d1779c597d3ed00
Emerson Nifa <emersongusta@gmail.com>
2022-06-19 11:54:22 -0300

    estrutura condicional if and else

--- js/script3.js
+++ js/script3.js
@@ -1,10 +1,23 @@
 // Condicionais 
 
-var hora = 9;
+var hora = 18;
 
-// start condição 
+// start condição  if and else simple
 if (hora < 12){
     console.log("Bom dia!");
-}else{
+}else if (hora < 18){
     console.log("Boa tarde")
+}else if (hora <= 23){
+    console.log("Boa Noite")
+}
+//////////////
+
+// start condição  if and else Logic &&
+if (hora >= 12 && hora < 18){
+    console.log("Boa tarde!");
+}
+
+// start condição  if and else Logic ||
+if (hora == 7 || hora == 12 || hora == 18){
+    console.log("Você está na hora do rush");
 }
